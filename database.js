e2f13fbd576d4845083d75c262a87492903200b1
Majid Rahimi <Majid88Rahimi@gmail.com>
2016-09-01 18:29:57 -0700

    impelemented search

--- database.js
+++ database.js
@@ -13,13 +13,48 @@ const Books = {
 }
 
 const Authors = {
-  create: name => db.one( 'INSERT INTO authors ( name ) VALUES ( $1 ) RETURNING id', [name] )
+  create: name => db.one( 'INSERT INTO authors ( name ) VALUES ( $1 ) RETURNING id', [name] ),
+  all: all => db.any( 'SELECT * FROM authors order by id asc' ),
+  forBooks: ids => db.any( 'SELECT * FROM authors JOIN book_authors ON book_authors.author_id=authors.id WHERE book_authors.book_id IN ($1:csv)', [ids] )
+}
+
+const Genres = {
+  forBooks: ids => db.any( 'SELECT * FROM genres JOIN book_genres ON book_genres.genre_id=genres.id WHERE book_genres.book_id IN ($1:csv)', [ids] )
 }
 
 const BookAuthors = {
-  create: (book_id, author_id) => db.one( 'INSERT INTO book_authors ( book_id, author_id ) VALUES ( $1, $2 ) RETURNING book_id', [ book_id, author_id ] )
+  create: (book_id, author_id) => db.one( 'INSERT INTO book_authors ( book_id, author_id ) VALUES ( $1, $2 ) RETURNING book_id', [ book_id, author_id ] ),
+  all: all => db.any( 'SELECT * FROM book_authors' )
+}
+
+const Search = {
+  byTitle: title => {
+    const sql = `SELECT * FROM books WHERE lower(title) LIKE '%${title}%'`
+
+    return db.any( sql )
+  },
+  byGenre: genreName => {
+    const sql = 
+      `SELECT books.* FROM books 
+       JOIN book_genres ON book_genres.book_id=books.id 
+       JOIN genres ON book_genres.genre_id=genres.id 
+       WHERE lower(genres.name) LIKE '%${genreName}%'`
+    console.log( sql )
+
+    return db.any( sql )
+  },
+  byAuthor: authorName => {
+    const sql = 
+      `SELECT books.* FROM books 
+       JOIN book_authors ON book_authors.book_id=books.id 
+       JOIN authors ON book_authors.author_id=authors.id 
+       WHERE lower(authors.name) LIKE '%${authorName}%'`
+    console.log( sql )
+
+    return db.any( sql )
+  },
 }
 
 module.exports = {
-  Books, Authors, BookAuthors
+  Books, Authors, BookAuthors, Genres, Search
 }
--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,62 @@
 const express = require('express')
 const router = express.Router()
 
-const Books = require( '../database' ).Books
+const { Books, Authors, Genres, Search } = require( '../database' )
 
 const PAGE_SIZE = 10
 
 router.get( '/', function(req, res, next) {
   const page = req.query.page || 1
+  const { search, type } = req.query
 
+  const bookFetch = ( search !== undefined && type !== undefined ) ?
+    getBooksSearch( search.toLowerCase(), type ) : getBooksPage
+
+    bookListing( bookFetch, page ).then( result => {
+      res.render('index', Object.assign( {}, result, { page } ))
+    })
+    .catch( error => res.send({ message: error.message, error }))
+})
+
+const getBooksSearch = (search, type) => page => {
+  return Search[ type ]( search )
+    .then( books => Promise.resolve([ books.length, books ]))
+}
+
+const getBooksPage = page =>
   Promise.all([
     Books.count(),
     Books.all( PAGE_SIZE * ( page - 1 ))
   ])
-  .then( result => res.render('index', { page, count: result[ 0 ].count, books: result[ 1 ] }) )
-  .catch( error => res.send({ message: error.message, error }))
-})
 
-module.exports = router
+const getAuthorsAndGenres = result => {
+  const [ countResult, books ] = result
+  const bookIds = books.map( book => book.id )
+
+  return Promise.all([
+    Promise.resolve( countResult.count ),
+    Promise.resolve( books ),
+    Authors.forBooks( bookIds ),
+    Genres.forBooks( bookIds )
+  ])
+}
+  
+const mergeBookFields = result => {
+  const [ count, rawBooks, bookAuthors, bookGenres ] = result
+
+  const books = rawBooks.map( book => {
+    const authors = bookAuthors.filter( author => author.book_id === book.id )
+    const genres = bookGenres.filter( genre => genre.book_id === book.id )
+
+    return Object.assign( {}, book, { authors, genres })
+  })
+
+  return Promise.resolve({ count, books })
+}
+
+const bookListing = (bookFetch, page) =>
+  bookFetch( page )
+    .then( getAuthorsAndGenres )
+    .then( mergeBookFields )
+
+module.exports = router 
